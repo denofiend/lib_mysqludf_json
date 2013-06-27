a46eea91899658242a4b7f82f3d7cf729c26417f
denofiend <denofiend@gmail.com>
2013-06-27 11:29:25 +0800

    remove create table sql

--- mx_trigger.sql
+++ mx_trigger.sql
@@ -43,8 +43,9 @@ CREATE TABLE `roll_transaction` (
   `type` int(11) DEFAULT NULL,
   `json` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`queue_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18088 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+/*
 CREATE TABLE `base_user_info_tmp` (
 	`user_id` int(11) NOT NULL,
 	`account` varchar(255) DEFAULT NULL,
@@ -100,12 +101,66 @@ CREATE TABLE `extend_user_info_tmp` (
 	KEY `BackupEmail` (`backup_email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 
+*/
 		
 DELIMITER $$
 		
 		
 CREATE TRIGGER mx_user_transfer AFTER INSERT ON extend_user_info
 FOR EACH ROW BEGIN
+
+	INSERT INTO `roll_transaction`(`user_id`, `type`, `json`) 
+		select a.`user_id`, 0 as `type`, 
+				json_object(
+					a.user_id as user_id,
+					a.account as account,
+					a.password as password,
+					a.nickname as nickname,
+					a.gender as gender,
+					a.status as status,
+					a.ip as register_ip,
+					a.register_time as register_time,
+					a.update_time as update_time,
+					a.language as language,
+					a.`from` as `from`,
+					a.`email` as email,
+					a.mobile as mobile,
+					a.country_code as country_code,
+					'cn' as region,
+					1 as id,
+					b.`first_name` as first_name,
+					b.`last_name` as last_name,
+					b.`birthday` as birthday,
+					b.`timezone` as timezone,
+					b.`location` as location,
+					b.`province` as province,
+					b.`city` as city,
+					b.`address` as address,
+					b.`zipcode` as zipcode,
+					b.`phone_number` as phone_number,
+					b.`mobile_number` as mobile_number,
+					b.`backup_email` as backup_email,
+					b.`avatarurl` as avatarurl,
+					b.`im_type` as im_type,
+					b.`im_value` as im_value,
+					b.`realname` as realname,
+					b.`home_location` as home_location,
+					b.`home_province` as home_province,
+					b.`home_city` as home_city,
+					b.`personalid` as personalid,
+					b.`qq` as qq,
+					0 as flag,
+					b.`skype` as skype
+				) as `json` from `base_user_info` a, `extend_user_info` b where a.`user_id` = NEW.`user_id` and b.`user_id` = a.`user_id`;
+
+END;
+$$
+
+
+--insert into base_user_info (user_id, nickname, account, email)values(1, 'b', 'b@a.com', 'b@a.com');
+--insert into extend_user_info (user_id)values(1);
+
+/*
 INSERT INTO base_user_info_tmp
 	select 
 	a.user_id,
@@ -148,55 +203,12 @@ INSERT INTO `extend_user_info_tmp`
 			b.`qq`,
 			0 as `flag`,
 			b.`skype`  from `extend_user_info` b where b.`user_id` = NEW.`user_id`;
+			*/
 
-	INSERT INTO `roll_transaction`(`user_id`, `type`, `json`) 
-		select a.`user_id`, 0 as `type`, 
-				json_object(
-					a.user_id as user_id,
-					a.account as account,
-					a.password as password,
-					a.nickname as nickname,
-					a.gender as gender,
-					a.status as status,
-					a.ip as register_ip,
-					a.register_time as register_time,
-					a.update_time as update_time,
-					a.language as language,
-					a.`from` as `from`,
-					a.`email` as email,
-					a.mobile as mobile,
-					a.country_code as country_code,
-					a.region as region,
-					a.id as id,
-					b.`first_name` as first_name,
-					b.`last_name` as last_name,
-					b.`birthday` as birthday,
-					b.`timezone` as timezone,
-					b.`location` as location,
-					b.`province` as province,
-					b.`city` as city,
-					b.`address` as address,
-					b.`zipcode` as zipcode,
-					b.`phone_number` as phone_number,
-					b.`mobile_number` as mobile_number,
-					b.`backup_email` as backup_email,
-					b.`avatarurl` as avatarurl,
-					b.`im_type` as im_type,
-					b.`im_value` as im_value,
-					b.`realname` as realname,
-					b.`home_location` as home_location,
-					b.`home_province` as home_province,
-					b.`home_city` as home_city,
-					b.`personalid` as personalid,
-					b.`qq` as qq,
-					b.`flag` as flag,
-					b.`skype` as skype
-				) as `json` from `base_user_info_tmp` a, `extend_user_info_tmp` b where a.`user_id` = NEW.`user_id` and b.`user_id` = a.`user_id`;
-
-END;
-$$
+-- alter table base_user_info add `id` bigint(20) NOT NULL;
+-- update base_user_info set id = 1;
+-- alter table base_user_info add `region` varchar(255) DEFAULT NULL;
+-- update base_user_info set region = 'com';
 
 
---insert into base_user_info (user_id, nickname, account, email)values(1, 'b', 'b@a.com', 'b@a.com');
---insert into extend_user_info (user_id)values(1);
 
